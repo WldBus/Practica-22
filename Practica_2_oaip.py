01042c99b0f53cc59d9699e70614450f9e6b1fb8
SnrTease <rapach567@gmail.com>
2024-12-19 19:35:49 +0300

    Добвлен список товаров

--- Practica_2_oaip.py
+++ Practica_2_oaip.py
@@ -2,3 +2,18 @@ users = [
     {"login": "user", "password": "user", "role": "user", "cart": [], "purchases": []},
     {"login": "HochuPelmeni227", "password": "YaAdmin120", "role": "admin"},
 ]
+
+products = [
+    {'product_name': 'Японская ручка', 'country': 'Япония', 'rating': 5.0, 'price': 119.59, 'clarity': 15},
+    {'product_name': 'Чипсы Takis', 'country': 'США', 'rating': 4.5, 'price': 215.99, 'clarity': 20},
+    {'product_name': 'Чипсы Doritos', 'country': 'США', 'rating': 4.6, 'price': 225.99, 'clarity': 10},
+    {'product_name': 'Консервмированный туман', 'country': 'Калифорния', 'rating': 3.7, 'price': 97.99, 'clarity': 5},
+    {'product_name': 'Раскрашенные черепа', 'country': 'Мексика', 'rating': 4.2, 'price': 110.59, 'clarity': 8},
+    {'product_name': 'Матрёшка', 'country': 'Россия', 'rating': 4.9, 'price': 89.99, 'clarity': 12},
+    {'product_name': 'Кукла "сарубобо"', 'country': 'Япония', 'rating': 5.0, 'price': 105.89, 'clarity': 12},
+    {'product_name': 'Кленовый сироп', 'country': 'Drama', 'rating': 3.3, 'price': 159.99, 'clarity': 18},
+    {'product_name': 'Камамбер', 'country': 'Франция', 'rating': 2.5, 'price': 359.99, 'clarity': 7},
+    {'product_name': 'Веер', 'country': 'Япония', 'rating': 4.2, 'price': 59.9, 'clarity': 7},
+]
+
+cart = {}
